08a2b3e4d7a40eddad8957e7f86d53cb42ab3892
RBusanic <43243405+RBusanic@users.noreply.github.com>
2019-12-11 22:45:02 -0500

    Add files via upload

---
+++ online-library/inc/data.php
@@ -0,0 +1,172 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Death Note: Another Note Los Angeles BB Murder Case",
+	"img" => "img/media/deathnoteanothernote.jpg",
+    "genre" => "Mystery",
+    "format" => "Hardcover",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Seamus Heaney"
+    ],
+    "publisher" => "Nisio Isin",
+    "isbn" => '9788863463095'
+];
+$catalog[102] = [
+    "title" => "RWBY: After the Fall",
+    "img" => "img/media/rwbyafterthefall.jpg",
+    "genre" => "Fantasy Fiction",
+    "format" => "Paperback",
+    "year" => 2019,
+    "category" => "Books",
+    "authors" => [
+        "E.C. Myers",
+				"Kerry Shawcross",
+				"Miles Luna",
+    ],
+    "publisher" => "Scholastics",
+    "isbn" => '97813383056'
+];
+$catalog[103] = [
+    "title" => "The Lovely Bones",
+    "img" => "img/media/Thelovelybones.jpg",
+    "genre" => "Novel",
+    "format" => "Paperback",
+    "year" => 2002,
+    "category" => "Books",
+    "authors" => [
+        "Alice Sebold"
+    ],
+    "publisher" => "Little, Brown",
+    "isbn" => '0316666343'
+];
+$catalog[104] = [
+    "title" => "Transformers: Exile",
+    "img" => "img/media/Transformersexile.jpg",
+    "genre" => "Science Fiction",
+    "format" => "Hardcover",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Alexander C. Irvine"
+    ],
+    "publisher" => "Del Rey Books",
+    "isbn" => '9780345519863'
+];
+//Movies
+$catalog[201] = [
+    "title" => "The Fifth Element",
+    "img" => "img/media/Fifthelement.jpg",
+    "genre" => "Action",
+    "format" => "DVD/Streaming",
+    "year" => 1997,
+    "category" => "Movies",
+    "director" => "Luc Besson",
+    "writers" => [
+        "Luc Besson",
+				"Robert Mark Kamen",
+    ],
+    "stars" => [
+        "Bruce Willis",
+        "Gary Oldman",
+        "Ian Holm",
+				"Chris Tucker",
+				"Milla Jovovich",
+    ]
+];
+$catalog[202] = [
+    "title" => "Harry Potter and the Goblet of Fire",
+    "img" => "img/media/Harrypottermovie4.jpg",
+    "genre" => "Fantasy",
+    "format" => "DVD/Streaming",
+    "year" => 2005,
+    "category" => "Movies",
+    "director" => "Mike Newell",
+    "writers" => [
+        "Steve Kloves",
+    ],
+    "stars" => [
+			"Daniel Radcliffe",
+			"Rupert Grint",
+			"Emma Watson",
+    ]
+];
+$catalog[203] = [
+    "title" => "My Neighbor Totoro",
+    "img" => "img/media/Myneighbortotoro.jpg",
+    "genre" => "Fantasy",
+    "format" => "DVD",
+    "year" => 1988,
+    "category" => "Movies",
+    "director" => "Hayao Miyazaki",
+    "writers" => [
+        "Hayao Miyazaki",
+    ],
+    "stars" => [
+			"Chika Sakamoto",
+			"Noriko Hidaka",
+			"Hitoshi Takagi",
+    ]
+];
+$catalog[204] = [
+    "title" => "The Secret of NIMH",
+		"img" => "img/media/Thesecretofnimh.jpg",
+	  "genre" => "Dark Fantasy Adventure",
+	  "format" => "DVD/Streaming",
+	  "year" => 1982,
+	  "category" => "Movies",
+	  "director" => "Don Bluth",
+	  "writers" => [
+	      "Don Bluth",
+				"Gary Goldman",
+				"John Pomeroy",
+				"Will Finn",
+	  ],
+	  "stars" => [
+	      "Elizabeth Hartman",
+	      "Dom DeLuise",
+	      "Derek Jacobi",
+				"Herimione Baddeley",
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Meteora",
+	"img" => "img/media/linkinpark.jpg",
+	"genre" => "Alternative",
+	"format" => "iTunes",
+	"year" => 2003,
+	"category" => "Music",
+	"artist" => "Linkin Park",
+];
+
+$catalog[302] = [
+	"title" => "All the Right Reasons",
+	"img" => "img/media/nickelback.jpg",
+	"genre" => "Canadian Rock",
+	"format" => "iTunes",
+	"year" => 2005,
+	"category" => "Music",
+	"artist" => "Nickelback",
+];
+$catalog[303] = [
+	"title" => "Synthesis",
+	"img" => "img/media/evanescence.png",
+	"genre" => "Goth-Pop",
+	"format" => "iTunes",
+	"year" => 2011,
+	"category" => "Music",
+	"artist" => "Evanescence",
+];
+$catalog[304] = [
+	"title" => "Rise",
+	"img" => "img/media/skillet.jpg",
+	"genre" => "Christen Metal",
+	"format" => "iTunes",
+	"year" => 2013,
+	"category" => "Music",
+	"artist" => "Skillet",
+];
+?>
---
+++ online-library/inc/data2.php
@@ -0,0 +1,213 @@
+<?php
+//Books
+$catalog[101] = [
+  "title" => "Death Note Another Note: The Los Angeles BB Murder Case",
+  "img" => "img/media/deathnoteanothernote.jpg",
+  "genre" => "Mystery",
+  "format" => "Hardcover",
+  "year" => 2008,
+  "category" => "Books",
+  "author" => [
+    "Nisio Isin"
+  ],
+  "translator" => [
+    "Andrew Cunningham"
+  ],
+  "publisher" => [
+    "Shueisha",
+    "Viz Media"
+  ],
+  "isbn" => '9788863463095'
+];
+
+$catalog[102] = [
+  "title" => "RWBY: After the Fall",
+  "img" => "img/media/rwbyafterthefall.jpg",
+  "genre" => "Fantasy Fiction",
+  "format" => "Paperback"
+  "year" => 2019,
+  "category" => "Books",
+  "authors" => [
+    "E.C. Myers",
+    "Kerry Shawcross",
+    "Miles Luna"
+  ]
+  "publisher" => "Scholastic",
+  "isbn" => '97813383056'
+];
+
+$catalog[103] = [
+  "title" => "Transformers: Exile",
+  "img" => "img/media/Transformersexile.jpg",
+  "genre" => "Science Fiction",
+  "format" => "Hardcover",
+  "year" => 2011,
+  "category" => "Books"
+  "author" => "Alexander C. Irvine",
+  "publisher" => "Del Rey Books",
+  "isbn" => '9780345519863',
+];
+
+$catalog[104] = [
+  "title" => "The Lovely Bones",
+  "img" => "img/media/Thelovelybones.jpg",
+  "genre" => "Novel",
+  "formats" => [
+    "Hardcover",
+    "Paperback",
+  ],
+  "year" => 2002,
+  "category" => "Books"
+  "author" => "Alice Sebold",
+  "publisher" => "Little, Brown",
+  "isbn" => '0316666343'
+];
+
+//Movies
+$catalog[201] = [
+  "title" => "The Fifth Element",
+  "img" => "img/media/Fifthelement.jpg",
+  "genre" => "Science Fiction Action"
+  "format" => [
+    "Blu-Ray"
+    "DVD"
+    "Digital"
+  ],
+  "year" => 1997,
+  "category" => "Movies",
+  "director" => "Luc Besson",
+  "writers" => [
+    "Luc Besson"
+    "Robert Mark Kamen"
+  ],
+  "stars" => [
+  "Bruce Willis"
+  "Gary Oldman"
+  "Ian Holm"
+  "Chris Tucker"
+  "Milla Jovovich"
+  ],
+];
+
+$catalog[202] = [
+  "title" => "Harry Potter and the Goblet of Fire",
+  "img" => "img/media/Harrypottermovie4.jpg",
+  "genre" => "Fantasy",
+  "format" => [
+    "Blu-Ray"
+    "DVD"
+    "Digital"
+  ],
+  "year" => 2005,
+  "category" => "Movies",
+  "director" => "Mike Newell",
+  "writers" => "Steve Kloves",
+  "stars" => [
+    "Daniel Radcliffe"
+    "Rupert Grint"
+    "Emma Watson"
+  ]
+];
+
+$catalog[203] = [
+  "title" => "My Neighbor Totoro",
+  "img" => "img/media/Myneighbortotoro.jpg",
+  "genre" => "Fantasy",
+  "format" => [
+    "DVD"
+    "Blu-Ray"
+  ],
+  "year" => 1988,
+  "category" => "Movies",
+  "director" => "Hayao Miyazaki",
+  "writers" => "Hayao Miyazaki",
+  "stars" => [
+    "Chika Sakamoto"
+    "Noriko Hidaka"
+    "Hitoshi Takagi"
+  ]
+];
+
+$catalog[204] = [
+  "title" => "The Secret of NIMH",
+  "img" => "img/media/Thesecretofnimh.jpg",
+  "genre" => "Dark Fantasy Adventure",
+  "format" => [
+    "Blu-Ray"
+    "DVD"
+    "Digital"
+  ],
+  "year" => 1982,
+  "category" => "Movies",
+  "director" => "Don Bluth",
+  "writers" => [
+    "Don Bluth"
+    "Gary Goldman"
+    "John Pomeroy"
+    "Will Finn"
+  ]
+  "stars" => [
+    "Elizabeth Hartman"
+    "Dom DeLuise"
+    "Derek Jacobi"
+    "Hermione Baddeley"
+  ]
+];
+
+//Music
+$catalog[301] = [
+  "title" => "Nickelback: All the Right Reasons",
+  "img" => "img/media/nickelback.jpg",
+  "genre" => "Canadian Rock"
+  "format" => [
+    "CD"
+    "iTunes"
+    "Amazon Music"
+  ],
+  "year" => 2005,
+  "category" => "Music",
+  "artist" => "Nickelback",
+];
+
+$catalog[302] = [
+  "title" => "Evanescence: Synthesis",
+  "img" => "img/media/evanescence",
+  "genre" => "Goth-Pop",
+  "format" => [
+    "CD"
+    "iTunes"
+    "Amazon Music"
+  ],
+  "year" => 2011,
+  "category" => "Music",
+  "artist" => "Evanescence",
+];
+
+$catalog[303] = [
+  "title" => "Linkin Park: Meteora",
+  "img" => "img/media/linkinpark.jpg",
+  "genre" => "Alternative Rock",
+  "format" => [
+    "CD"
+    "iTunes"
+    "Amazon Music"
+  ],
+  "year" => 2003,
+  "category" => "Music",
+  "artist" => "Linkin Park"
+];
+
+$catalog[304] = [
+  "title" => "Skillet: Rise",
+  "img" => "img/media/skillet.jpg",
+  "genre" => "Christen Metal",
+  "format" => [
+    "CD"
+    "iTunes"
+    "Amazon Music"
+  ]
+  "year" => 2013,
+  "category" => "Music",
+  "artist" => "Skillet"
+];
+  ?>
